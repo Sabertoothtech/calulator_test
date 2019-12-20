99b5a23f608c6fede26c1339331275d4440ad06c
sabertooth-adil <adil@sabertoothtech.in>
2019-12-19 18:37:24 -0800

    Adil: multiplication app

---
+++ adil_calculator/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AdilCalculatorConfig(AppConfig):
+    name = 'adil_calculator'
---
+++ adil_calculator/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse,JsonResponse
+
+def calculator(request):
+    return render(request,'calculator_adil.html')
+
+@csrf_exempt
+def caculateresult(request):
+    input1 = request.POST.get("input1")
+    input2 = request.POST.get("input2")
+
+    result = int(input1)*int(input2)
+
+    return JsonResponse({"result":result})
